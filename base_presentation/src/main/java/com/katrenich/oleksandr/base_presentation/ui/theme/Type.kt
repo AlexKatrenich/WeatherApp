69ee0e388ef3939c4cad5d5280ce94b350d65275
Oleksandr Katrenich <oleksandr@MacBook-Pro-Oleksandr.local>
2023-04-18 09:17:38 +0300

    Add background to details screen

--- base_presentation/src/main/java/com/katrenich/oleksandr/base_presentation/ui/theme/Type.kt
+++ base_presentation/src/main/java/com/katrenich/oleksandr/base_presentation/ui/theme/Type.kt
@@ -1,28 +1,43 @@
 package com.katrenich.oleksandr.base_presentation.ui.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.katrenich.oleksandr.base_presentation.R
 
 // Set of Material typography styles to start with
+val monseratRegular = FontFamily(Font(R.font.montserrat_regular))
+val monseratLight = FontFamily(Font(R.font.montserrat_light))
+
 val Typography = Typography(
+	defaultFontFamily = monseratRegular,
 	body1 = TextStyle(
-		fontFamily = FontFamily.Default,
+		fontFamily = monseratRegular,
 		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp
+		fontSize = 12.sp
+	),
+	h1 = TextStyle(
+		fontFamily = monseratLight,
+		fontWeight = FontWeight.Normal,
+		fontSize = 48.sp
+	),
+	h2 = TextStyle(
+		fontFamily = monseratLight,
+		fontWeight = FontWeight.Normal,
+		fontSize = 36.sp
+	),
+	h3 = TextStyle(
+		fontFamily = monseratLight,
+		fontWeight = FontWeight.Normal,
+		fontSize = 24.sp
+	),
+	caption = TextStyle(
+		fontFamily = monseratLight,
+		fontWeight = FontWeight.Normal,
+		fontSize = 18.sp
 	)
-	/* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
